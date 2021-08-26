202bb0941f9e8d5a63d7e0e3ae3d6ecc1f443399
Ricardo <ricoledan@gmail.com>
2021-08-26 09:45:29 -0400

    add middleware and auth token functionality

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 from typing import Optional
-from fastapi import FastAPI
+from functools import wraps
+import os
+from fastapi import Depends, FastAPI, Header, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 import uvicorn
 from dotenv import load_dotenv
 from pydantic import BaseModel
 
 load_dotenv()
-app = FastAPI()
 
+app = FastAPI()
 origins = ["*"]
 
 app.add_middleware(CORSMiddleware, allow_origins=origins)
@@ -17,15 +20,30 @@ class HealthCheck(BaseModel):
     status: str
 
 
+async def verify_token(x_access_token: str = Header(...)):
+    if x_access_token != os.getenv('ACCESS_TOKEN'):
+        raise HTTPException(
+            status_code=400, detail="x-access-token header invalid")
+
+
 @app.get('/')
 def root():
     return "Machine Learning REST API Service"
 
 
-@app.post("/api/healthcheck")
+@app.get('/api/model', dependencies=[Depends(verify_token)])
+def root(request: Request):
+    headers = request.headers
+    auth = headers.get('x-access-token')
+    print(auth)
+    return auth
+
+
+@ app.get("/api/logs", dependencies=[Depends(verify_token)])
+def read_logs():
+    return "TBA"
+
+
+@ app.post("/api/healthcheck")
 def healthcheck(hc: HealthCheck):
     return hc
-
-
-if __name__ == "__main__":
-    uvicorn.run("fastapi_code:app")
