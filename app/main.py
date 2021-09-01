da79a38af5a866dbab37bd3b7485023255e7a7eb
Ricardo <ricoledan@gmail.com>
2021-08-31 22:51:52 -0400

    refactor types out of main file

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
+from app.types.healthcheck import HealthCheck
 from typing import Optional
 import os
 from fastapi import Depends, FastAPI, Header, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from dotenv import load_dotenv
-from pydantic import BaseModel
 from middleware.verifytoken import verify_token
 from routes import models
 
@@ -14,12 +14,6 @@ app = FastAPI()
 origins = ["*"]
 
 app.add_middleware(CORSMiddleware, allow_origins=origins)
-
-
-class HealthCheck(BaseModel):
-    status: str
-
-
 app.include_router(models.router)
 
 
